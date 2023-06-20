c7aef2c4550ac85813e218840a6de1ce00af6d0e
TimiLi45 <137048750+TimiLi45@users.noreply.github.com>
2023-06-20 10:20:44 +0200

    Car v2

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -1,30 +1,81 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Xml.Serialization;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 using Random = System.Random;
 
 public class Car : MonoBehaviour
 {
     readonly TrafficManager _trafficManager;
 
+    enum Verhalten
+    {
+        none,
+        acceleration,
+        deceleration
+
+    }
+    Verhalten _verhalten = Verhalten.acceleration;
+
+
+
     bool    brake = false;
-    int     acceleration = 15;
-    int     deceleration = 15;
+    float     acceleration = .02f;
+    float     deceleration = 0.05f;
     int     maxSpeed = 120;
-    float   speed = 0;
+    float   speed = 0f;
+
+    float maxDistanceFront = 20f;
 
     int targetNodeID = -1;
     Node targetNode;
     Node currentNode;
-    //Vector3 targetLocation;
+    Vector3 targetLocation = new (100,0,0);
     //Vector3[] Waypoints;
 
     Street currentStreet;
+    private void Start()
+    {
+        Debug.Log("Star");
+    }
+
+
+
 
     private void Update()
     {
-            
+        
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, transform.forward * maxDistanceFront, Color.yellow);
+        Debug.Log(transform.forward * maxDistanceFront);
+        if (Physics.Raycast(transform.position, transform.forward * maxDistanceFront, out hit))
+        {
+            _verhalten = Verhalten.deceleration;
+           
+        }
+
+        transform.position =  Vector3.MoveTowards(transform.position, targetLocation, speed * Time.deltaTime);
+       
+        switch (_verhalten)
+        {
+            case Verhalten.none: break;
+            case Verhalten.acceleration:
+                {
+                    Acceleration();
+                    break;
+                }
+            case Verhalten.deceleration:
+                {
+                    Deceleration();
+                    break;
+                }
+        }
+
+
     }
 
     private void RandomStreet()
@@ -43,4 +94,31 @@ public class Car : MonoBehaviour
         }
         targetNode = possibleConnections[random.Next(possibleConnections.Count)].EndNode;
     }
+
+
+    private void Acceleration()
+    {
+        if (speed < maxSpeed)
+        {
+            brake = false;
+            speed = speed + acceleration;
+
+        }
+    }
+
+    private void Deceleration()
+    {
+        if (speed > 0)
+        {
+            speed = speed - deceleration;
+            if ( speed < 0)
+            {
+                speed = 0;
+            }
+
+        }
+    }
+
+
+
 }
